86601e337ac9a6e473a60bf50320e9655d5a0737
tasooo2004-spec <tasooo563@gmail.com>
2025-12-17 02:08:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -136,6 +136,12 @@ function renderOpinions(){
 }
 
 
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
 
 
 
